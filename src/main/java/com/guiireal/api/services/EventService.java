2f6f36048605e7d45ce3f0d00aed93afa5ab6fe7
guiireal <guii_real@hotmail.com>
2024-06-17 20:18:26 -0300

    wip

--- src/main/java/com/guiireal/api/services/EventService.java
+++ src/main/java/com/guiireal/api/services/EventService.java
@@ -1,15 +1,29 @@
 package com.guiireal.api.services;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.guiireal.api.domain.event.Event;
 import com.guiireal.api.domain.event.EventRequestDTO;
 
 @Service
 public class EventService {
+    @Autowired
+    private AmazonS3 s3Client;
+
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
     public Event createEvent(EventRequestDTO data) {
         String imageUrl = null;
 
@@ -28,7 +42,28 @@ public class EventService {
         return newEvent;
     }
 
-    private String uploadImage(MultipartFile image) {
-        return "";
+    private String uploadImage(MultipartFile multipartFile) {
+        String fileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+
+        try {
+            File file = this.convertMultiPartToFile(multipartFile);
+            s3Client.putObject(bucketName, fileName, file);
+
+            file.delete();
+
+            return s3Client.getUrl(bucketName, fileName).toString();
+        } catch(Exception e) {
+            System.out.println("Erro ao subir o arquivo: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private File convertMultiPartToFile(MultipartFile multipartFile) throws IOException {
+        File convertedFile = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream(convertedFile);
+        fos.write(multipartFile.getBytes());
+        fos.close();
+        
+        return convertedFile;
     }
 }
